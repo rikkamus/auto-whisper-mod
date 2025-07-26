32e31af14ca25c417e4bbad6e99d5df640b02c0f
rikkamus <rick@rikkamus.com>
2025-07-26 11:46:00 +0200

    Initial commit

---
+++ src/main/java/com/rikkamus/autowhisper/AutoWhisperMod.java
@@ -0,0 +1,18 @@
+package com.rikkamus.autowhisper;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.common.Mod;
+
+@Mod(AutoWhisperMod.MOD_ID)
+public class AutoWhisperMod {
+
+    public static final String MOD_ID = "autowhisper";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public AutoWhisperMod() {
+
+    }
+
+}
